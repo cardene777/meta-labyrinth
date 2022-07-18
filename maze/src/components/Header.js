0fb6912240551180a01b04ab9138cc5a85bcf908
heku777 <chotips08@gmail.com>
2022-07-18 23:12:39 +0900

    🛠 modify favicon.ico & menu

--- maze/src/components/Header.js
+++ maze/src/components/Header.js
@@ -153,10 +153,10 @@ export const Header = () => {
             <Disclosure.Panel className="sm:hidden">
                 <div className="px-2 pt-2 pb-3 space-y-1">
                 {navigation.map((item, index) => (
-                    <Disclosure.Button
+                    <Link
                         key={item.name}
-                        as="a"
-                        href={`https://labyrinze.com${item.href}`}
+                        // as="a"
+                        to={item.href}
                         className={classNames(
                             item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
                             'block px-3 py-2 rounded-md text-base font-md'
@@ -164,7 +164,7 @@ export const Header = () => {
                         aria-current={item.current ? 'page' : undefined}
                     >
                     {item.name}
-                    </Disclosure.Button>
+                    </Link>
                 ))}
                 </div>
             </Disclosure.Panel>
