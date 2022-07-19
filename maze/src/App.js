40fe0f332ebd9a0b682b34abdd02ce7d3d5f2338
heku777 <chotips08@gmail.com>
2022-07-19 21:26:04 +0900

    🛠 setting gop

--- maze/src/App.js
+++ maze/src/App.js
@@ -3,10 +3,12 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Home } from './components/Home';
 import { ListPage } from './components/ListPage';
 import { PageDetail } from './components/PageDetail';
+import Head from "./components/Head";
 
 function App() {
   return (
     <Router>
+      <Head title="Meta Labyrinth" />
       <div className="App">
         <Routes>
           <Route path="/" element={<Home />} />
